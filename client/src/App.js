f5fa08807fcac260dbd8d722718158a86e026cb9
ghaith <ghaith.diab@hotmail.com>
2023-01-21 13:49:43 +0100

    link front to back

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [data,setData]=useState("");
+
+
+const getData=async()=>{
+  const d=await axios.get('http://localhost:3000/home/getAllUsers')
+  setData(d.data)
+}
+  useEffect(()=>{
+    getData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {JSON.stringify(data)}
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import "dotenv/config.js"
 import userRouter  from './api/user.router.js';
-
+import cors from 'cors'
 // dotenv.config();
 const app=express();
+app.use(cors())
 app.use(express.json());
 app.use('/home',userRouter);
 
